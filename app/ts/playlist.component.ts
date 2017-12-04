33488bb049cf8b839899852558970cec50a4d612
Salman Zafar <salmanzafar949@gmail.com>
2017-12-04 15:08:53 +0500

    Created the playlist component for all the playlist

---
+++ app/ts/playlist.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+import {Videos} from "./videos";
+
+@Component({
+
+    selector:'playlist',
+    templateUrl:'app/ts/templates/playlist.component.html',
+})
+
+export class PlaylistComponent{}
